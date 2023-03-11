3dc77aff2ca89cf5f0ba4d296358b1fe5dfd09f1
josheng <engch28@gmail.com>
2023-03-12 03:30:51 +0800

    updated banner css attribute for smaller viewport

--- src/components/harleyfooter/harleyfooter.js
+++ src/components/harleyfooter/harleyfooter.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./harleyfooter.css";
 import harleylogo from "../images/haryleyfootimage.png";
 import harleyright from "../images/harleyright.png";
+import harleyslogo from "../images/harleylogo.png";
 
 const HarleyFooter = () => {
 
   return (
     <div className="harleybanner">
       <div className="image">
-        <img src={harleylogo} alt="Harley-Davidson Logo" />
+        <img src={harleylogo} alt="Harley-Davidson Logo" className="largeview" />
+        <img src={harleyslogo} alt="Harley-Davidson Logo" className="smallview" />
       </div>
       <div className="harley-text">
         <h2>To know more about Harley-Davidson motorcycles and products</h2>
